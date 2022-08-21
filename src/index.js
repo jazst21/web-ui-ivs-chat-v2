a3c4c3b3bbc93098bd8cea04b6f4dee69426f714
pugar <p.jayanegara@gmail.com>
2022-08-22 01:38:54 +0700

    unregister

--- src/index.js
+++ src/index.js
@@ -17,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://github.com/facebook/create-react-app/blob/master/packages/cra-template/template/README.md
-serviceWorker.register();
+serviceWorker.unregister();
