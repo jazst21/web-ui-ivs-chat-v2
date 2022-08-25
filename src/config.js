4d3d43a3bcde79e301e8e389ea2e3b9fe5e3dc6e
pugar <p.jayanegara@gmail.com>
2022-08-25 07:25:18 +0700

    ;

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 
 // Amazon IVS Playback URL
 // Replace this with your own Amazon IVS Playback URL
-export const PLAYBACK_URL = "https://29a1a5163b65.us-east-1.playback.live-video.net/api/video/v1/us-east-1.643021222550.channel.plsw4gEa1vJq.m3u8";";
+export const PLAYBACK_URL = "https://29a1a5163b65.us-east-1.playback.live-video.net/api/video/v1/us-east-1.643021222550.channel.plsw4gEa1vJq.m3u8";
 
 // Chat websocket address
 // The websocket endpoint for the chat room: wss://edge.ivschat.<AWS_REGION>.amazonaws.com
